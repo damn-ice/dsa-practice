8f63d3003ef73c0f87b195716503a286fa63a362
damn-ice <joelik18@gmail.com>
2023-02-10 19:37:26 +0100

    recursion

--- recursion.py
+++ recursion.py
@@ -1,3 +1,10 @@
+import math
+from itertools import product
+
+
+class BSTNode:
+    def __init__(self, data=None, left=None, right=None):
+        self.data, self.left, self.right = data, left, right
 
 def compute_tower_hanoi(num_rings: int) -> list[list]:
     NUM_PEGS = 3
@@ -71,7 +78,72 @@ def palindrome_decomposition(input: str) -> list[list]: #backtracking...
     palindrome_decomposition_helper(0, [])
     return result
 
-print(palindrome_decomposition("0204451881"))
+def generate_all_binary_tree(num_nodes: int) -> list[BSTNode]:
+    if num_nodes == 0:
+        return [None]
+
+    result = []
+    for num_left_tree_nodes in range(num_nodes):
+        num_right_tree_nodes = num_nodes - 1 - num_left_tree_nodes
+        left_subtree = generate_all_binary_tree(num_left_tree_nodes)
+        right_subtree = generate_all_binary_tree(num_right_tree_nodes)
+        # Generate all combination of left and right trees...
+        result += [BSTNode(0, left, right) for left in left_subtree for right in right_subtree]
+    return result
+
+sudoku = [
+    [5, 6, 0, 8, 4, 7, 0, 0, 0],
+    [3, 0, 9, 0, 0, 0, 6, 0, 0],
+    [0, 0, 8, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 8, 0, 0, 4, 0],
+    [7, 9, 0, 6, 0, 2, 0, 1, 8],
+    [0, 5, 0, 0, 3, 0, 0, 9, 0],
+    [0, 0, 0, 0, 0, 0, 2, 0, 0],
+    [0, 0, 6, 0, 0, 0, 8, 0, 7],
+    [0, 0, 0, 3, 1, 6, 0, 5, 9],
+]
+
+def solve_sudoku(partial_sudoku: list[list]) -> list[list]: #backtracking...
+    def solve_sudoku_helper(col: int, row: int):
+        if col == len(partial_sudoku): #base case...
+            col = 0 # move to begin of column and move to next row...
+            row += 1
+            if row == len(partial_sudoku):
+                return True
+
+        if partial_sudoku[col][row] != EMPTY_ENTRY:
+           return solve_sudoku_helper(col + 1, row)
+
+        def valid_sudoku(val: int, col: int, row: int) -> bool:
+            # check column...
+            if val in partial_sudoku[col]:
+                return False
+            # check row...
+            if any(val == partial_sudoku[k][row] for k in range(len(partial_sudoku))):
+                return False
+            # check region...
+            region_size = int(math.sqrt(len(partial_sudoku)))
+            i = col // region_size
+            j = row // region_size
+            return not any(val == partial_sudoku[region_size * i + a][region_size * j + b] for a, b in product(range(region_size), repeat=2))
+
+        for val in range(1, len(partial_sudoku) + 1):
+            if valid_sudoku(val, col, row):
+                partial_sudoku[col][row] = val
+                if solve_sudoku_helper(col + 1, row):
+                    return True
+        # No valid entry reset and continue previous function call loop...
+        partial_sudoku[col][row] = EMPTY_ENTRY
+        return False
+        
+    EMPTY_ENTRY = 0
+    solve_sudoku_helper(0, 0)
+    return partial_sudoku
+
+print(sudoku)
+print(solve_sudoku(sudoku))    
+# print(generate_all_binary_tree(5))
+# print(palindrome_decomposition("0204451881"))
 # print(generate_power_set([0, 1, 2]))
 # print(generate_permutation([2,3,5]))
 # print(n_queens(4))
