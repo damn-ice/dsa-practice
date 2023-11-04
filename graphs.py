c9a519b116cb1a14130777a64108d6365e1fad3f
damn-ice <joelik18@gmail.com>
2023-11-04 13:16:20 +0100

    connected components

--- graphs.py
+++ graphs.py
@@ -49,6 +49,44 @@ def breadthFirstTraversal(graph: dict[str, list], source: str):
             queue.put(neighbour)
 
 
+connected_component_graph = {
+    0: [8, 1, 5],
+    1: [0],
+    5: [0, 8],
+    8: [0, 5],
+    2: [3, 4],
+    3: [2, 4],
+    4: [3, 2],
+}
+
+
+def connectedComponents(graph: dict[int, list[int]]) -> int:
+    def depthFirstTraversal(graph: dict[str, list], start: str):
+        stack = [start]
+
+        while stack:
+            current = stack.pop()
+            if current not in visited:
+                visited.add(current)
+
+                # Current must exist on graph...
+                neighbours = graph.get(current)
+
+                for neighbour in neighbours:
+                    stack.append(neighbour)
+
+    visited = set()
+    count = 0
+
+    for node in graph:
+        if node not in visited:
+            depthFirstTraversal(graph, node)
+            count += 1
+
+    return count
+
+
 # depthFirstTraversal(graph, "a")
 # depthFirstTraversalRecursion(graph, "a")
-breadthFirstTraversal(graph, "a")
+# breadthFirstTraversal(graph, "a")
+print(connectedComponents(connected_component_graph))
